8e92a864c589c449312a4da05e28e485befcd486
ggow <dontouchmyfood@gmail.com>
2015-11-15 18:32:59 +0000

    Update product characteristics and product base

--- full_ford.mk
+++ full_ford.mk
@@ -5,9 +5,9 @@ $(call inherit-product, device/amazon/ford/device.mk)
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 
-PRODUCT_CHARACTERISTICS := phone,nosdcard
+PRODUCT_CHARACTERISTICS := tablet
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_NAME := full_ford
 PRODUCT_DEVICE := ford
