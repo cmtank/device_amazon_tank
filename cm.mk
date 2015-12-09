b1c37aa1b341f2aede5d18e1c09cd5ef5a69b49a
ggow <dontouchmyfood@gmail.com>
2015-12-09 13:01:50 +0000

    Removed NFC config

--- cm.mk
+++ cm.mk
@@ -1,9 +1,6 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration for Fire Phone
 $(call inherit-product, device/amazon/ford/full_ford.mk)
 
