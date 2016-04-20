3edb82591f0b718a45c96975b1781f2571648e6f
ggow <dontouchmyfood@gmail.com>
2016-04-20 10:23:44 +0100

    Remove selinux_permissive module from overlay-binaries

--- overlay-binaries/overlay-binaries.mk
+++ overlay-binaries/overlay-binaries.mk
@@ -1,10 +1,5 @@
 OVERLAY_BIN := device/amazon/ford/overlay-binaries
 
-# Install module to set selinux to permissive
-# Loaded by init.mt8127.rc
-PRODUCT_COPY_FILES += \
-	$(OVERLAY_BIN)/selinux_permissive.ko:system/lib/modules/vcodec_kernel_driver.ko
-
 # install-recovery.sh used to initialise init.d support	
 PRODUCT_COPY_FILES += \
 	$(OVERLAY_BIN)/install-recovery.sh:system/bin/new-install-recovery.sh \
