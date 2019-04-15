c919050aa198a99ba092732a1a1153bda6640678
ggow <joel.bacchus@protonmail.com>
2019-04-15 18:26:58 +0100

    Set selinux to permissive mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_CPU_SMP := true
 
 # Kernel Config
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x04000000 --tags_offset 0x00000100
-BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/amazon/ford
