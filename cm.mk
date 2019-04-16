e9ee5a48b46f5120eb9fc10892004a613a60f555
ggow <joel.bacchus@protonmail.com>
2019-04-16 20:57:41 +0100

    Fix derp

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit device configuration for Fire Phone
+# Inherit device configuration for full ford
 $(call inherit-product, device/amazon/ford/full_ford.mk)
 
 TARGET_SCREEN_WIDTH := 600
