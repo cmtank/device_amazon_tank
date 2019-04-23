3394ae7d35f59aa8ebd03312acafad021c5dfe9e
ggow <joel.bacchus@protonmail.com>
2019-04-23 17:32:45 +0100

    Use codec files from ROM instead of local tree

--- device.mk
+++ device.mk
@@ -40,13 +40,13 @@ PRODUCT_COPY_FILES += \
 
 # Config files
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(DEVICE_COMMON)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(DEVICE_COMMON)/configs/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    $(DEVICE_COMMON)/configs/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    $(DEVICE_COMMON)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(DEVICE_COMMON)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(DEVICE_COMMON)/configs/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg
-
 
 # Audio
 PRODUCT_PACKAGES += \
