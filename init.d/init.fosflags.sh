c51381b53f0777a46ca0c6e6ec9c99faa58a3a25
diego-cr <diegocr@gmail.com>
2019-08-21 22:31:08 +0200

    update init script

--- init.d/init.fosflags.sh
+++ init.d/init.fosflags.sh
@@ -1,7 +1,13 @@
 #!/system/bin/sh
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 
-sleep 10
-settings --user 0 put secure show_ime_with_hard_keyboard 1
+FILE="/data/.firstboot"
 
+if [ -f $FILE  ]; then
+  echo
+else
+  sleep 120
+  settings --user 0 put secure show_ime_with_hard_keyboard 1
+  echo 1 > $FILE
+fi
 
