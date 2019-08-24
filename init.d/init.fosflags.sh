7d1594ef5e6a148c90e7b290776d8927f45d240a
diego-cr <diegocr@gmail.com>
2019-08-24 21:37:37 +0200

    Bluetooth on at first boot

--- init.d/init.fosflags.sh
+++ init.d/init.fosflags.sh
@@ -8,6 +8,8 @@ if [ -f $FILE  ]; then
 else
   sleep 120
   settings --user 0 put secure show_ime_with_hard_keyboard 1
+  settings --user 0 put global bluetooth_on 1
+  service call bluetooth_manager 8
   echo 1 > $FILE
 fi
 
