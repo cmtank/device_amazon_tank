3cc55d1dd065051546f3b0de6ab2e9f9913aae48
diego-cr <diegocr@gmail.com>
2019-08-29 23:51:50 +0200

    Move thermal config to device

--- full_tank.mk
+++ full_tank.mk
@@ -74,6 +74,13 @@ PRODUCT_COPY_FILES += \
     device/amazon/tank/configs/media_profiles.xml:system/etc/media_profiles.xml \
     device/amazon/tank/configs/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg
 
+# Thermal
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf \
+     $(LOCAL_PATH)/configs/thermal.policy.conf:system/etc/.tp/thermal.policy.conf \
+     $(LOCAL_PATH)/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc \
+     $(LOCAL_PATH)/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf
+
 # Audio
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
