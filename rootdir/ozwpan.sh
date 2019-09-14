93a342b2ef32431fc3a799e078cc6d856cf420f7
diego-cr <diegocr@gmail.com>
2019-09-14 13:38:24 +0200

    tank: Update init scripts

---
+++ rootdir/ozwpan.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+readonly OZWPAN_DEV_FILE="/dev/ozwpan"
+readonly OZWPAN_SELECT_FILE="/sys/class/ozmo_wpan/ozwpan/select"
+readonly OZWPAN_MODE_FILE="/sys/class/ozmo_wpan/ozwpan/mode"
+PATH=/sbin:/system/sbin:/system/bin:/system/xbin
+
+while [ 1 ]; do
+    if [ -e ${OZWPAN_DEV_FILE} ]; then
+        chown root:amz_group ${OZWPAN_DEV_FILE} ${OZWPAN_SELECT_FILE} ${OZWPAN_MODE_FILE}
+        chmod 0660 ${OZWPAN_DEV_FILE} ${OZWPAN_SELECT_FILE} ${OZWPAN_MODE_FILE}
+        break
+    else
+        sleep 1
+    fi
+done
